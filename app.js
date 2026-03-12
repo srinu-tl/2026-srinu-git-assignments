c3d7aa7604390fe56ebb78e1f2a48ccf6d8fb550
srinu-tl <msrinu@tilicho.in>
2026-03-12 11:44:40 +0530

    error resolved

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // calculator-app/app.js - MODULE 1
 function calculate(num1, num2, operation) {
  console.log(`Calculating ${num1} ${operation} ${num2}`);
- 
+ if (opeation ==="exp" ){
+  return num1**num2;
+ }
  if (operation === "add"|| operation =="addition") {
  return num1 + num2; // Implement this
- 
+ }
  if (operation === "divide") {
  return num1 * num2; // Implement this
  }
@@ -15,7 +17,6 @@ function calculate(num1, num2, operation) {
  
  return "Operation not implemented";
 
-}
 }
 console.log(calculate(5, 3, "add")); // Must output: 8
 
